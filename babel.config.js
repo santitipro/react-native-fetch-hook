c6d845f0dce0acb437fa9b7542f08b22b9396ada
Santi López Bayo <slopez@MacBook-Pro-de-Santi.local>
2021-09-18 11:41:12 +0200

    add babel alias root

--- babel.config.js
+++ babel.config.js
@@ -6,9 +6,11 @@ module.exports = {
       {
         root: ['./src'],
         alias: {
+          '~': './',
           '@Screens': './src/screens',
           '@Components': './src/components',
           '@Navigation': './src/navigation',
+          '@Hooks': './src/hooks',
           '@Config': './src/config',
           '@Constants': './src/constants',
           '@Services': './src/services',
